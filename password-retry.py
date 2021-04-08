45ca4a8f8e4279a0f249945c7d429f22f672c0e1
RyanLin <Willie003652@gmail.com>
2021-04-08 21:04:00 +0800

    Basic vesion done done

--- password-retry.py
+++ password-retry.py
@@ -4,13 +4,13 @@
 # 最多輸入3次
 # 如果正確 就印出 "登入成功"
 # 如果不正確 就印出 "密碼錯誤! 還有_次機會!"
-j = 3
-password = 'a123456'
+j = 3 #剩餘次數
+password = 'a123456' 
 while True:
 	pwd = input('請輸入密碼：')
 	if pwd == password:
 		print('登入成功：')
-		break
+		break #跳出迴圈
 	else:
 		j = j - 1
 		print('密碼錯誤! 還有', j ,'次機會!')
