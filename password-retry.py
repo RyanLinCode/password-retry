6a613bbf66aebf137230d3fd28d6034e76fea636
RyanLin <Willie003652@gmail.com>
2021-04-09 11:34:27 +0800

    change if

--- password-retry.py
+++ password-retry.py
@@ -7,10 +7,14 @@
 j = 3 #剩餘次數
 password = 'a123456' 
 while j > 0:
+	j = j - 1
 	pwd = input('請輸入密碼：')
 	if pwd == password:
 		print('登入成功：')
 		break #跳出迴圈
 	else:
-		j = j - 1
-		print('密碼錯誤! 還有', j ,'次機會!')
+		print('密碼錯誤!')
+		if j > 0:
+			print('還有', j ,'次機會!')
+		else:
+			print('沒機會嘗試要鎖帳號了')
